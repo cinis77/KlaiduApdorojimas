62ae426297e3a8db9cb3b73fa3d9f27a2a4e96a3
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-10-03 21:08:21 +0300

    exception dalis

--- KlaiduApdorojimas/Program.cs
+++ KlaiduApdorojimas/Program.cs
@@ -6,10 +6,53 @@ using System.Threading.Tasks;
 
 namespace KlaiduApdorojimas
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private class ManoIsimtis : Exception
         {
+            public int MyProperty { get; set; }
+
+            public ManoIsimtis()
+            {
+            }
+
+            public ManoIsimtis(string message) : base(message)
+            {
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            string[] skaiciai = { "1", "-10", "1.8", "KK", "20" };
+            string a = null;
+            for (int i = 0; i < 6; i++)
+            {
+                try
+                {
+                    int skaicius = int.Parse(skaiciai[i]);
+                    Console.WriteLine(skaicius);
+                    if (i % 3 == 0)
+                    {
+                        throw new ManoIsimtis("Mano sugeneruotas exception");
+                    }
+                    if (a == null)
+                    {
+                        throw new Exception("Nesuinicializuota a");
+                    }
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Formatas netinkamas");
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine("Masyvas out of range");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
         }
     }
 }
